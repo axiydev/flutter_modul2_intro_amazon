3c84dc198fd4b914f5cb3729b62d49857a02a4bf
Axmadjon Isaqov <67532499+axiydev@users.noreply.github.com>
2022-09-14 11:21:19 +0500

    bug fixed

--- lib/page/sign_in/sign_in_page.dart
+++ lib/page/sign_in/sign_in_page.dart
@@ -49,6 +49,13 @@ class _SignInPageState extends State<SignInPage> with TickerProviderStateMixin {
   }
 
   @override
+  void dispose() {
+    _animationController!.dispose();
+    _animationControllerOne!.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
@@ -58,116 +65,118 @@ class _SignInPageState extends State<SignInPage> with TickerProviderStateMixin {
   }
 
   Widget get _foregroundWidget {
-    return Column(
-      children: [
-        Container(
-          height: _mediaQueryData!.size.height * 0.22,
-          width: _mediaQueryData!.size.width,
-          padding: const EdgeInsets.only(left: 20),
-          transform: Matrix4.translationValues(0, 28, 0),
-          decoration: const BoxDecoration(color: Colors.transparent),
-          alignment: Alignment.centerLeft,
-          child: AnimatedBuilder(
-              animation: _animationOffset,
-              builder: (context, _) {
-                return Transform.translate(
-                  offset: _animationOffset.value!,
-                  child: FadeTransition(
-                    opacity: _animationFade,
-                    child: const Text.rich(
-                      TextSpan(text: 'Login\n', children: [
-                        TextSpan(
-                          text: 'Welcome Back',
-                          style: TextStyle(fontSize: 18, color: Colors.white),
-                        )
-                      ]),
-                      style: TextStyle(fontSize: 25, color: Colors.white),
+    return RepaintBoundary(
+      child: Column(
+        children: [
+          Container(
+            height: _mediaQueryData!.size.height * 0.22,
+            width: _mediaQueryData!.size.width,
+            padding: const EdgeInsets.only(left: 20),
+            transform: Matrix4.translationValues(0, 28, 0),
+            decoration: const BoxDecoration(color: Colors.transparent),
+            alignment: Alignment.centerLeft,
+            child: AnimatedBuilder(
+                animation: _animationOffset,
+                builder: (context, _) {
+                  return Transform.translate(
+                    offset: _animationOffset.value!,
+                    child: FadeTransition(
+                      opacity: _animationFade,
+                      child: const Text.rich(
+                        TextSpan(text: 'Login\n', children: [
+                          TextSpan(
+                            text: 'Welcome Back',
+                            style: TextStyle(fontSize: 18, color: Colors.white),
+                          )
+                        ]),
+                        style: TextStyle(fontSize: 25, color: Colors.white),
+                      ),
                     ),
-                  ),
-                );
-              }),
-        ),
-        Expanded(
-            child: ClipRRect(
-          borderRadius: const BorderRadius.vertical(top: Radius.circular(60)),
-          child: ColoredBox(
-            color: Colors.white,
-            child: SingleChildScrollView(
-              child: Column(children: [
-                Padding(
-                  padding: EdgeInsets.only(
-                      top: _mediaQueryData!.size.height * 0.1,
-                      left: _mediaQueryData!.size.width * 0.1,
-                      right: _mediaQueryData!.size.width * 0.1),
-                  child: Card(
-                    elevation: 15,
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(15)),
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(15),
-                      child: Column(
-                        children: [
-                          _myTextField(hintText: 'Email', obscure: false),
-                          const Divider(
-                            thickness: .5,
-                            color: Colors.black,
-                          ),
-                          _myTextField(hintText: 'Password', obscure: true)
-                        ],
+                  );
+                }),
+          ),
+          Expanded(
+              child: ClipRRect(
+            borderRadius: const BorderRadius.vertical(top: Radius.circular(60)),
+            child: ColoredBox(
+              color: Colors.white,
+              child: SingleChildScrollView(
+                child: Column(children: [
+                  Padding(
+                    padding: EdgeInsets.only(
+                        top: _mediaQueryData!.size.height * 0.1,
+                        left: _mediaQueryData!.size.width * 0.1,
+                        right: _mediaQueryData!.size.width * 0.1),
+                    child: Card(
+                      elevation: 15,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(15)),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(15),
+                        child: Column(
+                          children: [
+                            _myTextField(hintText: 'Email', obscure: false),
+                            const Divider(
+                              thickness: .5,
+                              color: Colors.black,
+                            ),
+                            _myTextField(hintText: 'Password', obscure: true)
+                          ],
+                        ),
                       ),
                     ),
                   ),
-                ),
-                SizedBox(
-                  height: _mediaQueryData!.size.height * .04,
-                ),
-                Padding(
-                  padding: EdgeInsets.symmetric(
-                      horizontal: _mediaQueryData!.size.width * 0.2),
-                  child: SizedBox(
-                    width: double.infinity,
-                    child: _button(
-                        text: 'Login',
-                        onPressed: () {},
-                        color: const Color(0xFF2E7D31)),
+                  SizedBox(
+                    height: _mediaQueryData!.size.height * .04,
                   ),
-                ),
-                const Padding(
-                  padding: EdgeInsets.only(top: 20, bottom: 20),
-                  child: Text('Login with SNS'),
-                ),
-                Row(
-                  children: [
-                    const Spacer(
-                      flex: 1,
-                    ),
-                    Expanded(
-                      flex: 10,
+                  Padding(
+                    padding: EdgeInsets.symmetric(
+                        horizontal: _mediaQueryData!.size.width * 0.2),
+                    child: SizedBox(
+                      width: double.infinity,
                       child: _button(
-                          text: 'Facebook',
+                          text: 'Login',
                           onPressed: () {},
-                          color: Colors.blue),
+                          color: const Color(0xFF2E7D31)),
                     ),
-                    const Spacer(
-                      flex: 1,
-                    ),
-                    Expanded(
-                      flex: 10,
-                      child: _button(
-                          text: 'Github',
-                          onPressed: () {},
-                          color: Colors.black),
-                    ),
-                    const Spacer(
-                      flex: 1,
-                    ),
-                  ],
-                ),
-              ]),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.only(top: 20, bottom: 20),
+                    child: Text('Login with SNS'),
+                  ),
+                  Row(
+                    children: [
+                      const Spacer(
+                        flex: 1,
+                      ),
+                      Expanded(
+                        flex: 10,
+                        child: _button(
+                            text: 'Facebook',
+                            onPressed: () {},
+                            color: Colors.blue),
+                      ),
+                      const Spacer(
+                        flex: 1,
+                      ),
+                      Expanded(
+                        flex: 10,
+                        child: _button(
+                            text: 'Github',
+                            onPressed: () {},
+                            color: Colors.black),
+                      ),
+                      const Spacer(
+                        flex: 1,
+                      ),
+                    ],
+                  ),
+                ]),
+              ),
             ),
-          ),
-        ))
-      ],
+          ))
+        ],
+      ),
     );
   }
 
