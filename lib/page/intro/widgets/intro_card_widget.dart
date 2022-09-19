e0c53f8e91428e22585beae4fa09ac3b27e2a111
Axmadjon Isaqov <67532499+axiydev@users.noreply.github.com>
2022-09-19 11:15:05 +0500

    intro

--- lib/page/intro/widgets/intro_card_widget.dart
+++ lib/page/intro/widgets/intro_card_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:simple_animations/simple_animations.dart';
 
 class IntroCardWidget extends StatelessWidget {
   final String image;
@@ -10,35 +11,60 @@ class IntroCardWidget extends StatelessWidget {
       required this.subtitle,
       required this.title})
       : super(key: key);
-
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Image.asset(
-          image,
-          width: 300,
-        ),
-        Text(
-          title,
-          style: const TextStyle(
-              color: Colors.green, fontSize: 25, fontWeight: FontWeight.bold),
-        ),
-        const SizedBox(
-          height: 20,
-        ),
-        Padding(
-          padding: EdgeInsets.symmetric(
-              horizontal: MediaQuery.of(context).size.width * 0.2),
-          child: Text(
-            subtitle,
-            textAlign: TextAlign.center,
-            style: const TextStyle(
-                color: Colors.grey, fontSize: 16, fontWeight: FontWeight.w500),
+    return PlayAnimationBuilder<double?>(
+      fps: 60,
+      tween: Tween(begin: .0, end: 1),
+      duration: const Duration(milliseconds: 600),
+      builder: (context, value, _) => Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Transform.scale(
+            scale: value,
+            child: Image.asset(
+              image,
+              width: 300,
+            ),
           ),
-        ),
-      ],
+          const SizedBox(
+            height: 10,
+          ),
+          Transform.translate(
+            offset: Offset(0, -value! * 10),
+            child: Opacity(
+              opacity: value,
+              child: Text(
+                title,
+                style: const TextStyle(
+                    color: Colors.green,
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Transform.translate(
+              offset: Offset(0, -value * 10),
+              child: Opacity(
+                opacity: value,
+                child: Padding(
+                  padding: EdgeInsets.symmetric(
+                      horizontal: MediaQuery.of(context).size.width * 0.2),
+                  child: Text(
+                    subtitle,
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(
+                        color: Colors.grey,
+                        fontSize: 16,
+                        fontWeight: FontWeight.w500),
+                  ),
+                ),
+              ))
+        ],
+      ),
     );
   }
 }
